8507c5555c6247c3d9a957427a187915fd46724f
MabyDiaz <mabeldiaz7680@gmail.com>
2023-11-20 01:07:36 -0300

    agrego ProductoCard.css y background en el index.css

--- G4-FRONT/src/components/ProductoCard.jsx
+++ G4-FRONT/src/components/ProductoCard.jsx
@@ -1,26 +1,35 @@
-import { BsDisplay } from "react-icons/bs"
+//import { BsDisplay } from "react-icons/bs";
+import "./ProductoCard.css";
 
-export const ProductoCard = ({product}) => {
-
-    return (
-        <div className="card border-light" style={{width:'18rem'}}  >
-                <center>
-                <img src={product.imgUrl}  className="card-img-top "  style={{ width: 'auto', height: 200 }}   alt={`Image de ${product.name}`}/>
-                </center>
-            <div className="card-body">
-                <h5 className="card-title">{product.brand}</h5>
-                <p className="card-text">{product.model}</p>
-            </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">{product.description}</li>
-                <li className="list-group-item">${product.price}</li>
-                <li className="list-group-item">{product.dues} cuotas sin interes</li>
-                <li className="list-group-item">{product.discount}% off</li>
-            </ul>
-            <div className="card-body">
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
-        </div>
-    )
-}
+export const ProductoCard = ({ product }) => {
+  return (
+    <div className="card border-light" style={{ width: "24rem" }}>
+      <center>
+        <img
+          src={product.imgUrl}
+          className="card-img-top "
+          style={{ width: "auto", height: 200 }}
+          alt={`Image de ${product.name}`}
+        />
+      </center>
+      <div className="card-body">
+        <h5 className="card-title">{product.brand}</h5>
+        <p className="card-text">{product.model}</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">{product.description}</li>
+        <li className="list-group-item price">${product.price}</li>
+        <li className="list-group-item">{product.dues} cuotas sin interes</li>
+        <li className="list-group-item">{product.discount}% off</li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="card-link">
+          Card link
+        </a>
+        <a href="#" className="card-link">
+          Another link
+        </a>
+      </div>
+    </div>
+  );
+};
