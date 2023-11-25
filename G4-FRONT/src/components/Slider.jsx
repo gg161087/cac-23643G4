eeb315f0bdde9b58deaab41e47b57e8985cf951f
DanielAcosta2706 <danyel2706@gmail.com>
2023-11-25 20:08:02 -0300

    Agregando estilos al componente Header

--- G4-FRONT/src/components/Slider.jsx
+++ G4-FRONT/src/components/Slider.jsx
@@ -1,32 +1,32 @@
-import { Link } from "react-router-dom"
-import Carousel from "react-bootstrap/Carousel"
+import { Link } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
 
-import slide1 from "../assets/slider/slide1.jpg"
-import slide2 from "../assets/slider/slide2.webp"
-import slide3 from "../assets/slider/slide3.webp"
+import slide4 from "../assets/slider/slider4.jpg";
+import slide5 from "../assets/slider/slider5.jpg";
+import slide6 from "../assets/slider/slider6.jpg";
 
 import "./Slider.css";
 
 export const Slider = () => {
-    return (
-        <div className=" mt-4">
-            <Carousel>
-                <Carousel.Item className="carousel">
-                    <Link to="/celulares">
-                        <img className="imagen__carousel" src={slide1} alt="" />
-                    </Link>
-                </Carousel.Item>
-                <Carousel.Item className="carousel">
-                    <Link to="/televisores">
-                        <img className="imagen__carousel" src={slide3} alt="" />
-                    </Link>
-                </Carousel.Item>
-                <Carousel.Item className="carousel">
-                    <Link to="/computadoras">
-                        <img className="imagen__carousel" src={slide2} alt="" />
-                    </Link>
-                </Carousel.Item>
-            </Carousel>
-        </div>
-    );
+  return (
+    <div className="mt-4 ">
+      <Carousel>
+        <Carousel.Item className="carousel">
+          <Link to="">
+            <img className="imagen__carousel" src={slide4} alt="" />
+          </Link>
+        </Carousel.Item>
+        <Carousel.Item className="carousel">
+          <Link to="">
+            <img className="imagen__carousel" src={slide5} alt="" />
+          </Link>
+        </Carousel.Item>
+        <Carousel.Item className="carousel">
+          <Link to="">
+            <img className="imagen__carousel" src={slide6} alt="" />
+          </Link>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
--- G4-FRONT/src/partials/Header.jsx
+++ G4-FRONT/src/partials/Header.jsx
@@ -9,148 +9,137 @@ import { BsFillCartFill } from "react-icons/bs";
 
 import { SearchBar } from "../components/SearchBar";
 import { SearchResultsList } from "../components/SearchResultsList";
-import { getDinamic } from './../utils/getDinamic.js';
+import { getDinamic } from "./../utils/getDinamic.js";
 
 import logo from "/logo.png";
 import "./Header.css";
 
-export const Header = ({categories}) => {
+export const Header = ({ categories }) => {
+  const [results, setResults] = useState([]);
 
-    const [results, setResults] = useState([]);
-    
-    return (
-        <header className="header d-flex flex-wrap">
-            <Container fluid className="container__nav1">
-                <Container className="d-flex justify-content-between align-items-center p-2">
-                    <Nav className="">
-                        <Link
-                            className="nav1__link text-decoration-none ms-2 me-2"
-                            to="centro-de-ayuda"
-                        >
-                            <BiSolidMessage className="me-2" />
-                            Centro de ayuda
-                        </Link>
-                        <Link
-                            className="nav1__link text-decoration-none ms-2 me-2"
-                            to="venta-telefonica"
-                        >
-                            <FaPhoneVolume className="me-2" />
-                            Venta telefónica
-                        </Link>
-                        <Link
-                            className="nav1__link text-decoration-none ms-2 me-2"
-                            to="sucursales"
-                        >
-                            <SiHomeassistantcommunitystore className="me-2" />
-                            Sucursales
-                        </Link>
-                    </Nav>
-                    <Nav className="nav11">
-                        <Link
-                            className="nav1__link text-decoration-none ms-2 me-2"
-                            to="envios-domicilio"
-                        >
-                            <FaTruckMoving className="me-2 " />
-                            Envíos a domicilio
-                        </Link>
-                    </Nav>
-                </Container>
+  return (
+    <header className="header d-flex flex-wrap ">
+      <Container fluid className="container__nav1">
+        <Container className="d-flex justify-content-between align-items-center p-2 ">
+          <Nav className="">
+            <Link
+              className="nav1__link text-decoration-none ms-3 me-3 "
+              to="centro-de-ayuda"
+            >
+              <BiSolidMessage className="me-3" />
+              Centro de ayuda
+            </Link>
+            <Link
+              className="nav1__link text-decoration-none ms-3 me-3 "
+              to="venta-telefonica"
+            >
+              <FaPhoneVolume className="me-3" />
+              Venta telefónica
+            </Link>
+            <Link
+              className="nav1__link text-decoration-none ms-3 me-3 "
+              to="sucursales"
+            >
+              <SiHomeassistantcommunitystore className="me-3" />
+              Sucursales
+            </Link>
+          </Nav>
+          <Nav className="nav11">
+            <Link
+              className="nav1__link text-decoration-none ms-3 me-3 "
+              to="envios-domicilio"
+            >
+              <FaTruckMoving className="me-3 " />
+              Envíos a domicilio
+            </Link>
+          </Nav>
+        </Container>
+      </Container>
+      {/* ************************************************************************* */}
+      <Container fluid className="container__nav2">
+        <Container className="">
+          <Container className="d-flex align-items-center p-2">
+            <Nav className="">
+              <Link
+                className="nav2__logo d-flex align-items-center p-1 text-decoration-none"
+                to="/"
+              >
+                <img
+                  className=" me-3"
+                  src={logo}
+                  alt="Logo"
+                  style={{ width: "70px" }}
+                />
+                <h1>Argonauta</h1>
+              </Link>
+            </Nav>
+            <Container className="header__search">
+              <div className="search-bar-container">
+                <SearchBar setResults={setResults} />
+                {results && results.length > 0 && (
+                  <SearchResultsList results={results} />
+                )}
+              </div>
             </Container>
-            {/* ************************************************************************* */}
-            <Container fluid className="container__nav2">
-                <Container>
-                    <Container className="d-flex justify-content-between align-items-center p-2">
-                        <Nav className="">
-                            <Link
-                                className="nav2__logo d-flex align-items-center p-1 text-decoration-none"
-                                to="/"
-                            >
-                                <img
-                                    className=" me-3"
-                                    src={logo}
-                                    alt="Logo"
-                                    style={{ width: "70px" }}
-                                />
-                                <h1>Argonauta</h1>
-                            </Link>
-                        </Nav>
-
-                        <Container className="header__search">
-                            {/* <Form className="">
-                                        <Form.Control
-                                            type="search"
-                                            placeholder="Buscar producto"
-                                            className=""
-                                        />
-                                    </Form> */}
-                            {/* ******************************************************* */}
-                            <div className="search-bar-container">
-                                <SearchBar setResults={setResults} />
-                                {results && results.length > 0 && (
-                                    <SearchResultsList results={results} />
-                                )}
-                            </div>
-                            {/* ******************************************************* */}
-                        </Container>
-
-                        <Nav className="d-flex align-items-center">
-
-                            <Link
-                                className="miCuenta__link text-decoration-none ms-2 me-2"
-                                to="miCuenta"
-                            >
-                                <BiSolidUser /> Mi cuenta
-                            </Link>
-                            <Link
-                                className="miCuenta__link text-decoration-none ms-2 me-2"
-                                to="favoritos"
-                            >
-                                <AiFillHeart />
-                            </Link>
-                            <Link
-                                className="miCuenta__link text-decoration-none ms-2 me-2"
-                                to="miCarrito"
-                            >
-                                <BsFillCartFill />
-                            </Link>
-                        </Nav>
-                    </Container>
-                </Container>
-            </Container>
-            {/* ************************************************************************* */}
-            <Container fluid className="container__nav3">
-                <Container className="d-flex justify-content-between align-items-center p-2">
-                    {/* Esto estaba dentro del NavDropdown causando errores:  
-                        <NavDropdown.Item key={category.id}> 
-                            <Link className="nav3__link text-decoration-none ms-2 me-2" to={category.name}>{category.name}</Link> 
-                        </NavDropdown.Item>    
-                    */}
-
-                    <Nav className="">
-                        <NavDropdown className="nav3__link m-0 text-decoration-none" title="Categorías">
-                            {categories.map((category) =>(
-                                <Link key={category.id} className="nav3__link text-decoration-none ms-2 me-2" to={category.name}>{category.name}</Link>                                                                               
-                            ))}      
-                        </NavDropdown>                               
-                    </Nav>
-
-                    <Nav className="">
-                        <Link
-                            className="nav3__link text-decoration-none ms-2 me-2"
-                            to="exclusivo-online"
-                        >
-                            Exclusivos online
-                        </Link>
-                        <Link
-                            className="nav3__link text-decoration-none ms-2 me-2"
-                            to="ofertas"
-                        >
-                            Ofertas
-                        </Link>
-                    </Nav>
-
-                </Container>
-            </Container>
-        </header>
-    );
+            <Nav className="d-flex justify-content-end  w-50">
+              <Link
+                className="miCuenta__link text-decoration-none ms-3 me-3"
+                to="miCuenta "
+              >
+                <BiSolidUser /> Mi cuenta
+              </Link>
+              <Link
+                className="miCuenta__link text-decoration-none ms-3 me-3"
+                to="favoritos"
+              >
+                <AiFillHeart />
+              </Link>
+              <Link
+                className="miCuenta__link text-decoration-none ms-3 me-3"
+                to="miCarrito"
+              >
+                <BsFillCartFill />
+              </Link>
+            </Nav>
+          </Container>
+        </Container>
+      </Container>
+      {/* ************************************************************************* */}
+      <Container fluid className="container__nav3">
+        <Container className="d-flex justify-content-between align-items-center p-2">
+          <Nav className="">
+            <NavDropdown
+              className="nav3__link m-0 text-decoration-none"
+              title="Categorías"
+            >
+              {categories.map((category) => (
+                <NavDropdown.Item key={category.id}>
+                  <Link
+                    className="nav3__link text-decoration-none ms-3 me-3"
+                    to={category.name}
+                  >
+                    {category.name}
+                  </Link>
+                </NavDropdown.Item>
+              ))}
+            </NavDropdown>
+          </Nav>
+          <Nav className="">
+            <Link
+              className="nav3__link text-decoration-none ms-3 me-3"
+              to="exclusivo-online"
+            >
+              Exclusivos online
+            </Link>
+            <Link
+              className="nav3__link text-decoration-none ms-3 me-3"
+              to="ofertas"
+            >
+              Ofertas
+            </Link>
+          </Nav>
+        </Container>
+      </Container>
+    </header>
+  );
 };
