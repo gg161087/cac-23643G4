b23da13647a71572bfacbf7fc1ee1c793210ad74
MabyDiaz <mabeldiaz7680@gmail.com>
2023-12-12 18:35:10 -0300

    actualizo App.jsx, agrego ComprasOnline.jsx y css

--- G4-FRONT/src/App.jsx
+++ G4-FRONT/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Header } from "./partials/Header.jsx";
@@ -11,68 +11,81 @@ import CentroAyuda from "./pages/CentroAyuda.jsx";
 import { VentaTelefonica } from "./pages/VentaTelefonica.jsx";
 import { Sucursales } from "./pages/Sucursales.jsx";
 import { EnviosDomicilio } from "./pages/EnviosDomicilio.jsx";
-import { Login } from './pages/Login.jsx';
-import { ForgotPassword } from './pages/ForgotPassword.jsx';
-import { Register } from './pages/Register.jsx';
+import { Login } from "./pages/Login.jsx";
+import { ForgotPassword } from "./pages/ForgotPassword.jsx";
+import { Register } from "./pages/Register.jsx";
 import { MiCuenta } from "./pages/MiCuenta.jsx";
 import { Favoritos } from "./pages/Favoritos.jsx";
 import { MiCarrito } from "./pages/MiCarrito.jsx";
 import { ExclusivoOnline } from "./pages/ExclusivoOnline.jsx";
 import { Ofertas } from "./pages/Ofertas.jsx";
 import { ProductsFiltered } from "./pages/ProductsFiltered.jsx";
+import ComprasOnline from "./pages/ComprasOnline.jsx";
 
 export const App = () => {
-    const [allProducts, setAllProducts] = useState([]);
-    const [total, setTotal] = useState(0);
-    const [countProducts, setCountProducts] = useState(0);
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
 
-    return (
-        <BrowserRouter>
-            <Header 
+  return (
+    <BrowserRouter>
+      <Header
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <main>
+        <Routes>
+          <Route
+            index
+            element={
+              <Home
                 allProducts={allProducts}
                 setAllProducts={setAllProducts}
                 total={total}
                 setTotal={setTotal}
                 countProducts={countProducts}
-                setCountProducts={setCountProducts}                 
-             />
-            <main>
-                <Routes>
-                    <Route index element={<Home
-                        allProducts={allProducts}
-                        setAllProducts={setAllProducts}
-                        total={total}
-                        setTotal={setTotal}
-                        countProducts={countProducts}
-                        setCountProducts={setCountProducts} 
-                    />}>
-                    </Route>
-                    <Route path="/centro-de-ayuda" element={<CentroAyuda />} />
-                    <Route path="/venta-telefonica" element={<VentaTelefonica />}></Route>
-                    <Route path="/sucursales" element={<Sucursales />}></Route>
-                    <Route path="/envios-domicilio" element={<EnviosDomicilio />}></Route>
-                    <Route path="/login" element={<Login/>}></Route>
-                    <Route path="/forgot_password" element={<ForgotPassword/>}></Route>
-                    <Route path="/register" element={<Register/>}></Route>
-                    <Route path="/miCuenta" element={<MiCuenta />}></Route>
-                    <Route path="/favoritos" element={<Favoritos />}></Route>
-                    <Route path="/miCarrito" element={<MiCarrito />}></Route>
-                    <Route path="/products/:category/:id" element={<ProductDetail 
-                        allProducts={allProducts}
-                        setAllProducts={setAllProducts}
-                        total={total}
-                        setTotal={setTotal}
-                        countProducts={countProducts}
-                        setCountProducts={setCountProducts}                        
-                    />}>                        
-                    </Route>
-                    <Route path="/products/:category" element={<ProductsFiltered />}></Route>
-                    <Route path="/exclusivo-online" element={<ExclusivoOnline />}></Route>
-                    <Route path="/ofertas" element={<Ofertas />}></Route>
-                    <Route path="/*" element={<NotFound />}></Route>
-                </Routes>
-            </main>
-            <Footer />
-        </BrowserRouter>
-    );
+                setCountProducts={setCountProducts}
+              />
+            }
+          ></Route>
+          <Route path="/centro-de-ayuda" element={<CentroAyuda />} />
+          <Route path="/como-comprar-online" element={<ComprasOnline />} />
+          <Route path="/venta-telefonica" element={<VentaTelefonica />}></Route>
+          <Route path="/sucursales" element={<Sucursales />}></Route>
+          <Route path="/envios-domicilio" element={<EnviosDomicilio />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/forgot_password" element={<ForgotPassword />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/miCuenta" element={<MiCuenta />}></Route>
+          <Route path="/favoritos" element={<Favoritos />}></Route>
+          <Route path="/miCarrito" element={<MiCarrito />}></Route>
+          <Route
+            path="/products/:category/:id"
+            element={
+              <ProductDetail
+                allProducts={allProducts}
+                setAllProducts={setAllProducts}
+                total={total}
+                setTotal={setTotal}
+                countProducts={countProducts}
+                setCountProducts={setCountProducts}
+              />
+            }
+          ></Route>
+          <Route
+            path="/products/:category"
+            element={<ProductsFiltered />}
+          ></Route>
+          <Route path="/exclusivo-online" element={<ExclusivoOnline />}></Route>
+          <Route path="/ofertas" element={<Ofertas />}></Route>
+          <Route path="/*" element={<NotFound />}></Route>
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 };
---
+++ G4-FRONT/src/pages/ComprasOnline.jsx
@@ -0,0 +1,156 @@
+import Accordion from "react-bootstrap/Accordion";
+import "./ComprasOnline.css";
+
+const ComprasOnline = () => {
+  return (
+    <>
+      <div className="accordion-compras-online">
+        <h1 className="titulo-compras">Comprar Online</h1>
+        <Accordion accordion-compras-online>
+          <Accordion.Item eventKey="0" flush>
+            <Accordion.Header>¿CÓMO COMPRAR EN ITECHNOLOGY?</Accordion.Header>
+            <Accordion.Body>
+              Seleccioná tus productos Podés elegir los productos por
+              categorías, aplicando filtros por marca, precio y características
+              destacadas. Una vez que seleccionaste tu producto, hace clic en
+              “Comprar”. Serás redireccionado a “Mi carrito”. Carrito de compras
+              En el carrito verás el listado de productos pre-seleccionados
+              hasta el momento. Además, podrás calcular el monto de las cuotas y
+              el costo de envío. Haciendo clic en el botón “Continuar comprando”
+              podés seguir recorriendo nuestro sitio y agregar todos los
+              productos que desees. Si tenés un Cupón de Descuento, ingresalo en
+              este paso. Cuando completes la elección de productos, presioná el
+              botón “Finalizar Mi Pedido”. Seleccioná la Forma de Entrega Elegí
+              la modalidad de entrega de tu preferencia, podés optar entre
+              “Retiro en sucursal” (sin costo), Retiro en Punto de Entrega
+              Pickit, o “Envío a domicilio”. Seleccioná el Medio de Pago Elegí
+              el medio de pago y completá todos los datos solicitados. Si todos
+              los datos son verificados, tu compra se habrá realizado con éxito.
+              Factura digital y Notificaciones Cuando el pago haya sido
+              aprobado, recibirás en tu e-mail la factura digital. Además,
+              recibirás por la misma vía todas las notificaciones sobre el
+              estado de tu pedido.
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>
+              ¿CUÁLES SON LAS FORMAS DE PAGO DISPONIBLES?
+            </Accordion.Header>
+            <Accordion.Body>
+              Podés abonar tus compras online con los siguientes medios de pago:
+              Tarjetas de Crédito Tarjetas de Débito Mercado Pago Pago en
+              efectivo: a través de Rapipago o Pago Fácil. Para más información
+              sobre Formas de Pago disponibles, clic aquí.
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>
+              ¿CUÁLES SON LAS FORMAS DE ENTREGA PARA COMPRAS ONLINE?
+            </Accordion.Header>
+            <Accordion.Body>
+              Tenés disponible las siguientes opciones de entrega para tus
+              compras de Itechnology: Retiro en Sucursal: Esta modalidad de
+              entrega no tiene costo. Elegí en qué sucursal querés retirar tu
+              compra y cuando esté disponible te notificaremos para que pases a
+              retirar tu producto. Retiro en Punto Pickit: Esta modalidad de
+              entrega está disponible en productos seleccionados. Si el producto
+              que elegiste no tiene esta opción de entrega significa que no está
+              disponible para ser retirado por esta modalidad. Los envíos de
+              Pickit se realizan a través de un operador logístico externo y
+              tienen costo. Envío a Domicilio: Realizamos envíos a todo el país,
+              excepto a Tierra del Fuego, a través de operadores logísticos. Los
+              envíos a domicilio tienen costo adicional. Podrás calcular el
+              costo de envío en el carrito, cuando ingreses el código postal. Te
+              informaremos sobre los tiempos de entrega vigentes al seleccionar
+              la modalidad de envío a domicilio durante el proceso de compra.
+              Para más información sobre Entregas de tus Compras, clic aquí.
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>
+              ¿CÓMO HAGO EL SEGUIMIENTO DE MI COMPRA?:
+            </Accordion.Header>
+            <Accordion.Body>
+              Podés ver el estado actualizado de tu pedido desde tu cuenta,
+              ingresando a “Mis compras”. Seleccioná la compra sobre la que
+              querés realizar la consulta y hacé clic en “Seguir mi compra”. Si
+              seleccionaste la opción de “Envío a domicilio” o “Envío a Punto
+              Pickit” podés realizar el seguimiento del envío desde tu cuenta,
+              haciendo clic en el número de tracking del operador logístico que
+              aparece en el detalle de tu compra.
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>
+              ¿QUÉ HAGO SI TODAVÍA NO RECIBÍ MI COMPRA?
+            </Accordion.Header>
+            <Accordion.Body>
+              Si se ha vencido el plazo de entrega informado al momento de la
+              compra, podés comunicarte con nosotros completando el siguiente
+              Formulario. Te contactaremos para una respuesta a la mayor
+              brevedad posible. Recordá que podés hacer el seguimiento del envío
+              de tu compra desde tu cuenta, ingresando a “Mis compras”.
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="5">
+            <Accordion.Header>¿DÓNDE ENCUENTRO MI FACTURA?</Accordion.Header>
+            <Accordion.Body>
+              La factura de compra se enviará automáticamente a tu casilla de
+              e-mail cuando la transacción esté aprobada. No obstante, podés
+              descargarla desde tu cuenta, ingresando a “Mis compras”.
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="6">
+            <Accordion.Header>
+              ¿PUEDO OBTENER UNA FACTURA “A” DE MI COMPRA?
+            </Accordion.Header>
+            <Accordion.Body>
+              Sí, podés solicitar Factura A. Durante el proceso de compra,
+              cuando solicitemos los datos de facturación, marcá la opción
+              Factura A y completá con los datos de la empresa. La factura se
+              enviará automáticamente a tu casilla de e-mail cuando la
+              transacción esté aprobada.
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="7">
+            <Accordion.Header>
+              ¿MI COMPRA ESTÁ GARANTIZADA Y PROTEGIDA?
+            </Accordion.Header>
+            <Accordion.Body>
+              En Itechnology te garantizamos la seguridad de todas tus compras
+              online, manteniendo tus datos bajo la más estricta
+              confidencialidad. Gracias al respaldo de SSL (Secure Socket Layer)
+              el sistema de seguridad utilizado por Itechnology, te aseguramos
+              cada transacción electrónica que realices en nuestra página web.
+              Tu información personal será cifrada y no podrá ser leída ni
+              utilizada por terceros. A su vez, Itechnology te asegura que tus
+              datos personales se manejan con absoluta confidencialidad y
+              conforme a lo dispuesto por la legislación vigente. Nuestro sitio
+              cuenta con certificados de seguridad y cifrado SSL brindados por
+              DigiCert. También asociados y auditados por la Cámara Argentina de
+              Comercio Electrónico bajo el sello e-confianza.com. Para mayor
+              información, visitá Términos y Condiciones
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <div className="volver-container">
+          <button
+            className="volver-button"
+            onClick={() => (window.location.href = "/centro-de-ayuda")}
+          >
+            Volver
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ComprasOnline;
